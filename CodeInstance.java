5a1554c60954f18f7f2f158ddb50f520207c0fa7
makfazlic <makfazlic@protonmail.com>
2021-05-16 17:24:14 +0200

    first commit

---
+++ CodeInstance.java
@@ -0,0 +1,18 @@
+/**
+ * A single Latex command 
+ */
+public class CodeInstance {
+
+    private String value;
+
+    public CodeInstance(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
+
+
+
