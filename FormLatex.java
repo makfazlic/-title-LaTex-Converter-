bffc5a19d39783bc3cc83df731f75451b8526e50
Feeeerro <ferruccio.robi6@gmail.com>
2021-06-02 10:05:28 +0200

    checkstyle error fixed

--- FormLatex.java
+++ FormLatex.java
@@ -1,4 +1,7 @@
-import java.io.*;  // Import the IOException class to handle errors
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
